3f7365aad9f2d8c620058ddcf11311afa203d6e3
qquebak-ai <qquebak@gmail.com>
2026-07-15 06:48:22 +0300

    Delete src/supabaseClient.ts

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "ВАШ_PROJECT_URL";
-const supabaseAnonKey = "ВАШ_ANON_KEY";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
