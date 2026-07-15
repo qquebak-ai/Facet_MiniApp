bcee2382ea866833fac9309d98ed2f596ce98135
qquebak-ai <qquebak@gmail.com>
2026-07-15 07:53:13 +0300

    Delete src/supabaseClient.ts

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://rinxzaakkhxdbhjghtwa.supabase.co";
-const supabaseAnonKey = "sb_publishable_wtYIk727NN5RA4Io7p2WAw_3xWWj9v6";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
